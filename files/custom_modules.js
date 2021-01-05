15aca736402aa36f727263b19236c0468cb2c4d0
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-05 16:06:18 -0300

    custom modules

--- files/custom_modules.js
+++ files/custom_modules.js
@@ -7,8 +7,6 @@ custom_module_manager.validator = function (custom_modules, type) {
     // Validate
     if(
         custom_modules &&
-        typeof custom_modules.path === "string" &&
-        custom_modules.path.length > 0 &&
         custom_modules[type] &&
         (Array.isArray(custom_modules[type].custom) || Array.isArray(custom_modules[type].default))
     ) {
@@ -24,9 +22,6 @@ custom_module_manager.validator = function (custom_modules, type) {
 
 // Run
 custom_module_manager.run = async function (custom_modules, hookType) {
-    
-    const path = require('path');
-    const fs = require('fs');
 
     // Run Custom Modules
     const run_custom_module = async function (type) {
@@ -35,18 +30,13 @@ custom_module_manager.run = async function (custom_modules, hookType) {
         for (const item in custom_modules[hookType][type]) {
 
             // Is String
-            if (typeof custom_modules[hookType][type][item] === "string") {
+            if (typeof custom_modules[hookType][type][item] === "function") {
 
                 // Try to Read the Module
                 try {
 
-                    // Module FIle Path
-                    const file_path = path.join(custom_modules.path, './' + custom_modules[hookType][type][item]);
-
-                    // Read File
-                    if(fs.lstatSync(file_path).isFile()) {
-                        await require(file_path)(db_prepare, hookType);
-                    }
+                    // Run Script
+                    await custom_modules[hookType][type][item](db_prepare, hookType);
                 
                 }
 
@@ -62,7 +52,7 @@ custom_module_manager.run = async function (custom_modules, hookType) {
             else {
 
                 // Prepare Error Message
-                const err = new Error(`The Custom Paypal Module value needs to be a string!\nArray: ${type}\nIndex: ${item}`);
+                const err = new Error(`The Custom Paypal Module value needs to be a Function!\nArray: ${type}\nIndex: ${item}`);
 
                 console.error(err);
                 console.error(err.message);
